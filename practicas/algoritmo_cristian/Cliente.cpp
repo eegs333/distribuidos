b935582ef1ce14fb71aaa020514b897c5ec16287
Elías García <elias.garcia@gft.com>
2017-04-05 08:27:38 -0800

    cliente servidor online

--- practicas/algoritmo_cristian/Cliente.cpp
+++ practicas/algoritmo_cristian/Cliente.cpp
@@ -6,25 +6,40 @@
 #include <strings.h>
 #include <arpa/inet.h>
 #include <cstring>
+#include <time.h>
+#include <unistd.h>
+#include <sys/time.h>
 #include "PaqueteDatagrama.h"
 #include "SocketDatagrama.h"
 int puerto = 7200;
-// lol
+// lolstruct timeval t1;
 int main(int argn, char* args[])
 {
-
+    struct timeval now;
+    struct timeval t0;
+    struct timeval t1;
+    struct timeval time;
+    struct tm* ptm;
+	  char time_string[40];
+	  long milliseconds;
     if (argn >= 2) {
         SocketDatagrama socket(sizeof(now));
         PaqueteDatagrama paqueteEnviado((char *)&now, sizeof(now), args[1], puerto);
+        gettimeofday (&t0, NULL);
         socket.envia(paqueteEnviado);
-        PaqueteDatagrama paqueteRecibido(sizeof(int));
-        printf("Esperando paquete\n");
+        PaqueteDatagrama paqueteRecibido(sizeof(now));
         socket.recibe(paqueteRecibido);
-        printf("Paquete recibido\n");
-        //printf("%d\n", *((int*)paqueteRecibido.obtieneDatos()));
-        struct timeval now;
+        gettimeofday (&t1, NULL);
         memcpy(&now, paqueteRecibido.obtieneDatos(), sizeof(now));
         printf("%ld\n", now.tv_usec);
+        printf("%ld\n", now.tv_sec);
+        timersub(t1.tv_sec,t0.tv_sec)
+        time.tv_sec=(-)/2 + now.tv_sec;
+        time.tv_usec=(t1.tv_usec-t0.tv_usec)/2 + now.tv_usec;
+        ptm = localtime (&time.tv_sec);
+    	  strftime (time_string, sizeof (time_string), "%Y-%m-%d %H:%M:%S", ptm);
+    	  milliseconds = time.tv_usec / 1000;
+    	  printf ("%s.%03ld\n", time_string, milliseconds);
     } else {
         printf("Uso: %s ip\n",args[0]);
     }
--- practicas/algoritmo_cristian/Servidor.cpp
+++ practicas/algoritmo_cristian/Servidor.cpp
@@ -15,25 +15,38 @@ int main(int argc, char** argv) {
 		struct timeval tv;
 	  struct tm* ptm;
 	  char time_string[40];
+		char time_hr[10];
 	  long milliseconds;
 		char IP[] = "127.0.0.1";
-
-	  gettimeofday (&tv, NULL);
+		//obtiene tiempo
+		gettimeofday (&tv, NULL);
 		printf("%ld\n",tv.tv_sec);
 		printf("%ld\n",tv.tv_usec);
+		//formato tiempo
 	  ptm = localtime (&tv.tv_sec);
 	  strftime (time_string, sizeof (time_string), "%Y-%m-%d %H:%M:%S", ptm);
-	  milliseconds = tv.tv_usec / 1000;
+		strftime (time_hr, sizeof (time_hr), "%H", ptm);
+		milliseconds = tv.tv_usec / 1000;
 	  printf ("%s.%03ld\n", time_string, milliseconds);
+		printf ("%s\n", time_hr);
     SocketDatagrama socket(puerto);
 		PaqueteDatagrama paqueteRecibido(sizeof(tv));
-		//PaqueteDatagrama paqueteEnviado(out,1,IP,puerto);
 		PaqueteDatagrama paqueteEnviado((char *)&tv,sizeof(tv),IP,puerto);
 		printf("Esperando mensajes...\n");
 		while (1) {
-      		printf("%d\n",socket.recibe(paqueteRecibido));
-					paqueteEnviado.inicializaPuerto(paqueteRecibido.obtienePuerto());
-					socket.envia(paqueteEnviado);
-					printf("ENVIANDO DATOS\n" );
+      		//printf("%d\n",socket.recibe(paqueteRecibido));
+					if (socket.recibe(paqueteRecibido)){
+						gettimeofday (&tv, NULL);
+						printf("%ld\n",tv.tv_sec);
+						printf("%ld\n",tv.tv_usec);
+						ptm = localtime (&tv.tv_sec);
+					  strftime (time_string, sizeof (time_string), "%Y-%m-%d %H:%M:%S", ptm);
+					  milliseconds = tv.tv_usec / 1000;
+					  printf ("%s.%03ld\n", time_string, milliseconds);
+						PaqueteDatagrama paqueteEnviado((char *)&tv,sizeof(tv),IP,puerto);
+						paqueteEnviado.inicializaPuerto(paqueteRecibido.obtienePuerto());
+						socket.envia(paqueteEnviado);
+						printf("ENVIANDO DATOS\n" );
+					}
     }
 }
