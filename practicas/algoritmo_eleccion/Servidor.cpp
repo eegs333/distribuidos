3df9807a613154e3465b11c428ee8c810d48c400
Elías García <elias.garcia@gft.com>
2017-04-18 14:35:55 -0800

    avance

--- practicas/algoritmo_eleccion/Servidor.cpp
+++ practicas/algoritmo_eleccion/Servidor.cpp
@@ -8,44 +8,84 @@
 #include "PaqueteDatagrama.h"
 #include "SocketDatagrama.h"
 #include "gfxModified.h"
+#include <iostream>
 
+using namespace std;
 int puerto = 7200;
 
-void screen()
+void screen(char * argv[])
 {
 	int ysize = 300;
-	int xsize = 300;
-	int x = 100;
+	int xsize = 1300;
+	int x = 0 ;
 	int y = 25;
-	int factor = 20;
-	int ascii = 69;
+	int factor;
+	int ascii;
 	char c;
-
-	gfx_open(xsize,ysize,"Graphics Program");
+	gfx_open(xsize,ysize,"Mensajes");
 	gfx_color(0,200,100);
-	gfx_display_ascii(x, y , factor, ascii);
-	while(1) {
-		// Wait for the user to press a character.
-		c = gfx_wait();
+	//Generar IP
+	char* ip = argv[3];
+	for(char* it = ip; *it; ++it) {
+		factor = 7;
+		y = 25;
+		ascii = int(*it);
+		if(ascii == 46){
+			factor = 1;
+			y = 100;
+		}
+		cout << *it << " = " << ascii << endl;
+		x += 70;
+		gfx_display_ascii(x, y , factor, ascii);
+	}
+	//Generar mensaje
+	x = 0;
+	char* str = argv[2];
+	for(char* its = str; *its; ++its) {
+		factor = 3;
 
-		// Quit if it is the letter q.
-		if(c=='q') break;
+		y = 150;
+		ascii = int(*its);
+		x += 70;
+		gfx_display_ascii(x, y , factor, ascii);
 	}
 
+
+	while(1) {
+		c = gfx_wait();
+		if(c=='q') break;
+	}
 }
 
-
-
-int main(void) {
-	screen();
-  /*  SocketDatagrama socket(puerto);
-    PaqueteDatagrama paqueteRecibido(65536);
-    PaqueteDatagrama paqueteEnviado(out,1,IP,puerto);
+void grandulon(char * IP[]){
+	char msj = "ELECCION";
+	for(int i = 0; i > sizeof(IP); i++){
+		cout << IP[i] << endl;
+		PaqueteDatagrama paqueteEnviado(msj,sizeof(msj),IP[i],puerto);
+		if(paqueteRecibido.obtieneDatos() == "OK"){
+			cout << "Coordinador: " << IP[i] <<  endl;
+		}
+	}
+}
+int main(int argc, char * argv[]) {
+		char out [30];
+		char in [30];
+		char *IP[15];
+		for(int i = 1; i < argc; i++){
+				IP[i] = argv[i];
+				cout << IP[i] << endl;
+		}
+    SocketDatagrama socket(puerto);
     printf("Esperando mensajes...\n");
-
+		//screen(argv);
     while (1) {
-        socket.recibe(paqueteRecibido));
-        paqueteEnviado.inicializaPuerto(paqueteRecibido.obtienePuerto());
-        socket.envia(paqueteEnviado);
-    }*/
+			PaqueteDatagrama paqueteRecibido(sizeof(out));
+				if(paqueteRecibido.obtieneDatos()){
+
+				}
+	    PaqueteDatagrama paqueteEnviado(out,sizeof(out),paqueteRecibido.obtieneDireccion(),puerto);
+			socket.recibe(paqueteRecibido);
+      paqueteEnviado.inicializaPuerto(paqueteRecibido.obtienePuerto());
+      socket.envia(paqueteEnviado);
+    }
 }
